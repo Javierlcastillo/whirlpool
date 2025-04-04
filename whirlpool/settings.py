970f1150515c834b20477fbaf25cf1528f848cd3
Diego Lumbreras <73485642+Lumbreras2306@users.noreply.github.com>
2025-04-04 02:58:39 -0600

    fix: added allowed hosts

--- whirlpool/settings.py
+++ whirlpool/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'django-insecure-your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '20.246.91.21']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
