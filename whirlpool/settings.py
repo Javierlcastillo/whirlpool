c19caee855407eb278291d08a2e10d0af8df68c8
Diego Lumbreras <73485642+Lumbreras2306@users.noreply.github.com>
2025-04-22 23:45:25 -0600

    allowed all CORS

--- whirlpool/settings.py
+++ whirlpool/settings.py
@@ -167,6 +167,25 @@ REST_FRAMEWORK = {
 # CORS settings
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
 # Swagger/OpenAPI Settings
 SWAGGER_SETTINGS = {
